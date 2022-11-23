fa1400b642e918a7ed9a5f2365d91ac28560b517
Jina Kim <jinakimcool@gmail.com>
2022-11-23 12:07:08 -0600

    Update header.html

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-//    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,7 +11,6 @@ if(empty($_POST['name'])  		||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-// $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
